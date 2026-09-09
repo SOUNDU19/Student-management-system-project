8748648be06f5b79146d78b0f86c411bdaf51212
SOUNDU19 <ssoundarya090@gmail.com>
2026-09-09 21:00:36 +0530

    Add MySQL database connection

---
+++ app/database/database.py
@@ -0,0 +1,27 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
+
+DATABASE_URL = URL.create(
+    drivername="mysql+pymysql",
+    username=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host="localhost",
+    port=3306,
+    database="student_management"
+)
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
