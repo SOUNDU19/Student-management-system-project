dc55f420b253e44634d782e4a914091ee9532b79
SOUNDU19 <ssoundarya090@gmail.com>
2026-09-09 22:28:08 +0530

    Add MySQL database connection

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+
 from app.database.database import engine, Base
 from app.models.student import Student
+from app.routes.student import router as student_router
+
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+app.include_router(student_router)
 
 
 @app.get("/")
---
+++ app/routes/student.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.database.database import get_db
+from app.models.student import Student
+from app.schemas.student import StudentCreate, StudentResponse
+
+
+router = APIRouter()
+
+
+@router.post("/students", response_model=StudentResponse)
+def create_student(
+    student: StudentCreate,
+    db: Session = Depends(get_db)
+):
+    new_student = Student(
+        name=student.name,
+        email=student.email,
+        age=student.age,
+        department=student.department
+    )
+
+    db.add(new_student)
+    db.commit()
+    db.refresh(new_student)
+
+    return new_student
